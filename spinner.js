97eebc5ae9ca4548174a24f540228cc0de21847e
yamanaka kazuki <yamanakakazuki@yamanakaikkinoMacBook-Air.local>
2022-12-23 22:27:44 +0900

    増減機能実装完了

---
+++ spinner.js
@@ -0,0 +1,41 @@
+// spinner
+
+$(function() {
+  $('.spinner').each(function() {
+    var el  = $(this);
+    var add = $('.spinner-add');
+    var sub = $('.spinner-sub');
+
+    // substract
+    el.parent().on('click', '.spinner-sub', function() {
+      if (el.val() > parseInt(el.attr('min'))) {
+        el.val(function(i, oldval) {
+          return --oldval;
+        });
+      }
+      // disabled
+      if (el.val() == parseInt(el.attr('min'))) {
+        el.prev(sub).addClass('disabled');
+      }
+      if (el.val() < parseInt(el.attr('max'))) {
+        el.next(add).removeClass('disabled');
+      }
+    });
+
+    // increment
+    el.parent().on('click', '.spinner-add', function() {
+      if (el.val() < parseInt(el.attr('max'))) {
+        el.val(function(i, oldval) {
+          return ++oldval;
+        });
+      }
+      // disabled
+      if (el.val() > parseInt(el.attr('min'))) {
+        el.prev(sub).removeClass('disabled');
+      }
+      if (el.val() == parseInt(el.attr('max'))) {
+        el.next(add).addClass('disabled');
+      }
+    });
+  });
+});
